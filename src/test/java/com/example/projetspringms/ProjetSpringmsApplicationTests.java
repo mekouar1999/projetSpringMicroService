35aa5a3bf416a4db390d71ed7b6e186f5f671b00
mekouar1999 <othman.mekouar@etu.univ-cotedazur.fr>
2023-01-31 11:44:44 +0100

    init

---
+++ src/test/java/com/example/projetspringms/ProjetSpringmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetspringms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
